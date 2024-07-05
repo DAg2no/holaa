c75dcdead5b88386815706f4518c076b88c1c0a7
DAg2no <dualwinder777.2.0@gmail.com>
2024-07-04 22:19:24 -0400

    main

--- src/script.js
+++ src/script.js
@@ -1,1 +1,6 @@
-// nada por aca
+const hamburger = document.querySelector(".drawer");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+})
